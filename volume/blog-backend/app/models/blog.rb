427d6cf90d1c22e65bcbf4d06bfab358440c6558
edimo <edimossilva@gmail.com>
2020-03-03 20:47:02 +1300

    Adding factory bot

--- volume/blog-backend/app/models/blog.rb
+++ volume/blog-backend/app/models/blog.rb
@@ -1,4 +1,3 @@
 class Blog < ApplicationRecord
   validates_presence_of :name
-  validates_presence_of :is_private
 end
--- volume/blog-backend/spec/models/blog_spec.rb
+++ volume/blog-backend/spec/models/blog_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  it "is invalid when blog is empty" do
+  let!(:complete_blog) { build :blog }
+
+  it "is valid when blog has name" do
+    expect(complete_blog).to be_valid
+  end
+
+  it "is invalid when blog has no name" do
     expect(Blog.new).to be_invalid
   end
 end
