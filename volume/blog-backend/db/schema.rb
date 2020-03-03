5b629740871c86034d3cb32a07a25d50ca0d16e7
edimo <edimossilva@gmail.com>
2020-03-03 23:24:38 +1300

    adding user

--- volume/blog-backend/db/schema.rb
+++ volume/blog-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_071350) do
+ActiveRecord::Schema.define(version: 2020_03_03_095909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_03_03_071350) do
   create_table "blogs", force: :cascade do |t|
     t.string "name"
     t.boolean "is_private"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ volume/blog-backend/spec/models/user_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let!(:user) { build :user }
+  let!(:user_without_username) { build :user, :without_username }
+  let!(:user_without_password) { build :user, :without_password }
+  let!(:user_with_invalid_password) { build :user, :without_password }
+  
+  it "is valid when user has all valid fields" do
+    expect(user).to be_valid
+  end
+
+  context 'when user is invalid' do
+    it "is invalid when user has no username" do
+      expect(user_without_username).to be_invalid
+    end
+    it "is invalid when user has no password" do
+      expect(user_without_password).to be_invalid
+    end
+    it "is invalid when user has password with length lesser then 3" do
+      expect(user_without_password).to be_invalid
+    end 
+    
+    context 'when username already ' do
+      let!(:existing_user) { create :user }
+      it 'is invalid when user_name already exists' do
+        new_user = build :user
+        new_user.username = existing_user.username
+        expect(new_user).to be_invalid
+      end
+    end
+  end
+end
