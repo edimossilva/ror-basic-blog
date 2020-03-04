478a12132aad9f548a31aee87c59974c304472a0
edimo <edimossilva@gmail.com>
2020-03-04 22:01:12 +1300

    refactoring authorizations

--- volume/blog-backend/app/controllers/application_controller.rb
+++ volume/blog-backend/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  before_action :authorize_request
   def authorize_request
     header = request.headers['Authorization']
     header = header.split(' ').last if header
@@ -9,6 +10,15 @@ class ApplicationController < ActionController::API
       render json: { errors: e.message }, status: :unauthorized
     rescue JWT::DecodeError => e
       render json: { errors: e.message }, status: :unauthorized
+    end
+  end
+
+  def public_request
+    header = request.headers['Authorization']
+    if header
+      header = header.split(' ').last
+      @decoded = JsonWebToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
     end
   end
 
